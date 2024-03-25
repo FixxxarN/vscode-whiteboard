5960202975d315d4170449eaf2711f8528f4aab0
FixxxarN <tornstromandre@gmail.com>
2024-03-25 18:19:02 +0100

    Add blank canvas when open whiteboard tab

---
+++ src/extension.js
@@ -0,0 +1,49 @@
+const vscode = require('vscode');
+const { getNonce } = require('./utils');
+
+function activate(context) {
+	let currentPanel = undefined;
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-whiteboard.start', () => {
+		const columnToShowIn = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+
+		if (currentPanel) {
+			currentPanel.reveal(columnToShowIn);
+		}
+		else {
+			currentPanel = vscode.window.createWebviewPanel('vscode-whiteboard', 'Whiteboard', columnToShowIn || vscode.ViewColumn.One, { enableScripts: true, localResourceRoots: [context.extensionUri] });
+		}
+
+		currentPanel.webview.html = getWebviewContent(context.extensionUri, currentPanel);
+	}))
+}
+
+function getWebviewContent(extensionUri, currentPanel) {
+	const whiteboardUri = currentPanel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'src', 'whiteboard.js'))
+	const stylesUri = currentPanel.webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'src', 'styles.css'))
+
+	const nonce = getNonce();
+
+	return `
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Visual Studio Code - Whiteboard</title>
+			<link href="${stylesUri}" rel="stylesheet">
+			<script nonce="${nonce}" src="${whiteboardUri}" defer></script>
+		</head>
+		<body>
+			<canvas id="whiteboard"></canvas>
+		</body>
+	</html>
+	`
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
---
+++ src/whiteboard.js
@@ -0,0 +1,6 @@
+// @ts-nocheck
+const canvas = document.getElementById('whiteboard');
+
+canvas.style.width = '100%';
+canvas.style.height = '100%';
+
