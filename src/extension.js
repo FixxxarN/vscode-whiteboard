da33e525defae2f25f2cd34d6b03d0117a48c6df
FixxxarN <tornstromandre@gmail.com>
2024-04-02 18:29:56 +0200

    Ugly fix to remove console error message exports is not defined

--- src/extension.js
+++ src/extension.js
@@ -34,6 +34,7 @@ function getWebviewContent(extensionUri, currentPanel) {
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<title>Visual Studio Code - Whiteboard</title>
 			<link href="${stylesUri}" rel="stylesheet">
+			<script>var exports = {};</script>
 			</head>
 			<body>
 			<div class="interactives-container">
